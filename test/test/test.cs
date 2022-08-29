c31032e395991c2b1c1c0b3a2f2e319751d5ae7d
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-08-29 11:45:51 +0900

    2진수, 10진수, 16진수

--- test/test/test.cs
+++ test/test/test.cs
@@ -6,19 +6,22 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // 데이터 + 로직
-            // 체력 0
-            
             //byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
             //sbyte(1바이트 -128~128), ushort(2바이트 0~6만), uint(4바이트 0~43억), ulong
-            int hp;
-            short level = 100;
-            long id;
-            hp = 100;
-
-            byte attack = 0;
-            attack--;
-            Console.WriteLine("Hello Number! {0}",attack);
+            
+            //10진수
+            //0~9
+                //10
+                
+            //2진수
+            //0 1
+                //0b00 0b01 0b10 0b11 0b100
+                
+            //16잔수
+            //0~9 a b c d e f
+                //0x00 0x01 0x02 ... 0x0F
+            
+            //Console.WriteLine("Hello Number! {0}",attack);
         }
     }
 }
