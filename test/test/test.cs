24e3671e79c4515cd07b06e04d2b658665064e37
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-02 16:06:20 +0900

    비트 연산

--- test/test/test.cs
+++ test/test/test.cs
@@ -6,25 +6,16 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int hp = 100;
-            int level = 50;
-            bool isAlive = (hp > 0);
-            bool isHighLevel = (level >= 40);
-            
-            //&& AND    ||OR    ! NOT
-            //a=살아있는 고랩 유저인가요?
-            bool a = isAlive && isHighLevel;
+            int id = 123;
+            int key = 401;
 
-            //b = 살아있거나, 고랩 유저이거나, 둘 중 하나인가요?
-            bool b = isAlive || isHighLevel;
-            
-            //c = 죽은 유저인가요?
-            bool c = !isAlive;
+            int a = id ^ key;
+            int b = a ^ key;
+
+            // <<   >>    &(and)  |(or)    &(xor)   ~(not)
             
             Console.WriteLine(a);
             Console.WriteLine(b);
-            Console.WriteLine(c);
-            
         }
     }
 }
