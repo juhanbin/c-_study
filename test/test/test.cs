97a183cc052ebf0af28a1c730c1ce19d0146f96b
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-03 18:03:45 +0900

    삼항연산자

--- test/test/test.cs
+++ test/test/test.cs
@@ -6,26 +6,15 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int choice = 0; //0:가위   1:바위    2:보     3.치트키
+            // 삼항연산자
+            int number = 25;
 
-            switch (choice)
-            {
-                case 0:
-                    Console.WriteLine("가위입니다");
-                    break;
-                case 1:
-                    Console.WriteLine("바위입니다");
-                    break;
-                case 2:
-                    Console.WriteLine("보입니다");
-                    break;
-                case 3:
-                    Console.WriteLine("치트키입니다");
-                    break;
-                default:
-                    Console.WriteLine("다 실패했습니다");
-                    break;
-            }
+            bool isPair = ((number % 2) == 0 ? true : false);
+
+            if ((number % 2) == 0)
+                isPair = true;
+            else
+                isPair = false;
         }
     }
 }
