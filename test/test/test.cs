e3f5c33e49e323f2360dfa1feaa1ac6aad67aee5
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-03 17:58:58 +0900

    switch문

--- test/test/test.cs
+++ test/test/test.cs
@@ -8,14 +8,24 @@ namespace CSharp
         {
             int choice = 0; //0:가위   1:바위    2:보     3.치트키
 
-            if (choice == 0)
-                Console.WriteLine("가위입니다");
-            else if (choice == 1)
-                Console.WriteLine("바위입니다");
-            else if (choice == 2)
-                Console.WriteLine("보입니다");
-            else
-                Console.WriteLine("치트키입니다");
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("가위입니다");
+                    break;
+                case 1:
+                    Console.WriteLine("바위입니다");
+                    break;
+                case 2:
+                    Console.WriteLine("보입니다");
+                    break;
+                case 3:
+                    Console.WriteLine("치트키입니다");
+                    break;
+                default:
+                    Console.WriteLine("다 실패했습니다");
+                    break;
+            }
         }
     }
 }
