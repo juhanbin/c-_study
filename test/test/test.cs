4ec6c6362a18aad8cc6741d3d3995788be96b336
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-06 22:11:21 +0900

    while

--- test/test/test.cs
+++ test/test/test.cs
@@ -4,55 +4,18 @@ namespace CSharp
 {
     class Program
     {
-        enum Choice
-        {
-            Rock=1,
-            Paper = 2,
-            Scissors = 0
-        }
         static void Main(string[] args)
         {
-            //0:가위  1:바위    2:보
-
-            Random rand = new Random();
-            int aiChoice = rand.Next(0, 3); //0~2사이의 랜덤 값
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
+            //거울아 거울아
+            string answer;
+            do
             {
-                case (int)Choice.Scissors:
-                    Console.WriteLine("당신의 선택은 가위입니다.");
-                    break;
-                case (int)Choice.Rock:
-                    Console.WriteLine("당신의 선택은 바위입니다.");
-                    break;
-                case (int)Choice.Paper:
-                    Console.WriteLine("당신의 선택은 보입니다.");
-                    break;
-            }
+                Console.WriteLine("강사님은 잘생기셨나요?(y/n)");
+                answer = Console.ReadLine();
+            } 
+            while (answer != "y");
             
-            switch (aiChoice)
-            {
-                case (int)Choice.Scissors:
-                    Console.WriteLine("컵퓨터의 선택은 가위입니다.");
-                    break;
-                case (int)Choice.Rock:
-                    Console.WriteLine("컵퓨터의 선택은 바위입니다.");
-                    break;
-                case (int)Choice.Paper:
-                    Console.WriteLine("컵퓨터의 선택은 보입니다.");
-                    break;
-            }
-            
-            //승리 무승부 패배
-            if(choice==aiChoice)
-                Console.WriteLine("무승부 입니다");
-            else if((choice==(int)Choice.Scissors && aiChoice==(int)Choice.Paper)
-                    ||(choice==(int)Choice.Rock && aiChoice==(int)Choice.Scissors)
-                    ||(choice==(int)Choice.Paper && aiChoice==(int)Choice.Rock))
-                Console.WriteLine("당신이 이겼습니다.");
-            else Console.WriteLine("컴퓨터가 이겼습니다");
+            Console.WriteLine("정답입니다");
         }
     }
 }
