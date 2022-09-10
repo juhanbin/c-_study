67b3dbf9d0aca0a0988ff9c319e1e3ce465a7ce5
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-11 02:25:29 +0900

    함수

--- test/test/test.cs
+++ test/test/test.cs
@@ -4,15 +4,18 @@ namespace CSharp
 {
     class Program
     {
+        //메소드   함수
+        static void AddOne(ref int number)
+        {
+            number++;
+        }
+        
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
-            {
-                if (i % 3 != 0)
-                    continue;
-                
-                Console.WriteLine($"3으로 나누는 숫자 발견{i}");
-            }
+            //복사(짭퉁)    참조(진퉁)
+            int a = 0;
+            Program.AddOne(ref a);
+            Console.WriteLine($"결과는 {a}");
         }
     }
 }
