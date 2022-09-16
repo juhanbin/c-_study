f29bda820dded78714f0e45119912dc61985c650
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-16 23:59:06 +0900

    TextRPG 직업선택

--- test/test/test.cs
+++ test/test/test.cs
@@ -1,18 +1,46 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace CSharp
 {
     class Program
     {
-        static int Add(int a, int b)
+        enum ClassType
         {
-            int ret = a + b;
-            return ret;
+            None = 0,
+            Knight=1,
+            Archer=2,
+            Mage=3
         }
         static void Main(string[] args)
         {
-            int ret=Program.Add(10, 20);
-            Console.WriteLine(ret);
+            ClassType choice = ClassType.None;
+            
+            while (true)
+            {
+                Console.WriteLine("직업을 선택하세요");
+                Console.WriteLine("[1] 기사");
+                Console.WriteLine("[2] 궁수");
+                Console.WriteLine("[3] 법사");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        choice = ClassType.Knight;
+                        break;
+                    case "2":
+                        choice = ClassType.Archer;
+                        break;
+                    case "3":
+                        choice = ClassType.Mage;
+                        break;
+                }
+
+                if (choice != ClassType.None)
+                    break;
+            }
         }
     }
 }
