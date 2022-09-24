79ae1c66e226c5dccf5e75011979b0ad78f610a4
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-24 23:39:15 +0900

    객체지향의 시작

--- test/test/test.cs
+++ test/test/test.cs
@@ -1,158 +1,34 @@
 using System;
+using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
 
 namespace CSharp
 {
+    class Knight
+    {
+        public int hp;
+        public int attack;
+
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Attack");
+        }
+    }
     class Program
     {
-        enum ClassType
-        {
-            None = 0,
-            Knight=1,
-            Archer=2,
-            Mage=3
-        }
-
-        enum MonsterType
-        {
-            None=0,
-            Slime=1,
-            Orc=2,
-            Skeleton=3
-        }
-
-        struct Monster
-        {
-            public int hp;
-            public int attack;
-        }
-
-        struct Player
-        {
-            public int hp;
-            public int attack;
-        }
-
-        static ClassType ChooseClass()
-        {
-            Console.WriteLine("직업을 선택하세요");
-            Console.WriteLine("[1] 기사");
-            Console.WriteLine("[2] 궁수");
-            Console.WriteLine("[3] 법사");
-
-            ClassType choice = ClassType.None;
-            string input = Console.ReadLine();
-            switch (input)
-            {
-                case "1":
-                    choice = ClassType.Knight;
-                    break;
-                case "2":
-                    choice = ClassType.Archer;
-                    break;
-                case "3":
-                    choice = ClassType.Mage;
-                    break;
-            }
-
-            return choice;
-        }
-
-        static void CreatPlayer(ClassType choice, out Player player)
-        {
-            switch (choice)
-            {
-                case ClassType.Knight:
-                    player.hp = 100;
-                    player.attack = 10;
-                    break;
-                case ClassType.Archer:
-                    player.hp = 75;
-                    player.attack = 12;
-                    break;
-                case ClassType.Mage:
-                    player.hp = 50;
-                    player.attack = 15;
-                    break;
-                default:
-                    player.hp = 0;
-                    player.attack = 0;
-                    break;
-            }
-        }
-
-        static void CreateRandomMonster(out Monster monster)
-        {
-            Random rand = new Random();
-            int randMonster = rand.Next(1, 4);
-            switch (randMonster)
-            {
-                case (int)MonsterType.Slime:
-                    Console.WriteLine("슬라임이 스폰되었습니다");
-                    monster.hp = 20;
-                    monster.attack = 4;
-                    break;
-                case (int)MonsterType.Orc:
-                    Console.WriteLine("오크이 스폰되었습니다");
-                    monster.hp = 40;
-                    monster.attack = 4;
-                    break;
-                case (int)MonsterType.Skeleton:
-                    Console.WriteLine("스켈레톤이 스폰되었습니다");
-                    monster.hp = 30;
-                    monster.attack = 3;
-                    break;
-                default:
-                    monster.hp = 0;
-                    monster.attack = 0;
-                    break;
-            }
-        }
-
-        static void EnterField()
-        {
-            Console.WriteLine("필드에 접속했습니다");
-
-            Monster monster;
-            CreateRandomMonster(out monster);
-
-            Console.WriteLine("[1] 전투모드로 돌입");
-            Console.WriteLine("[2] 일정확률로 마을로 도망");
-        }
-        static void EnterGame()
-        {
-            while (true)
-            {
-                Console.WriteLine("마을에 접속했습니다");
-                Console.WriteLine("[1] 필드로 간다");
-                Console.WriteLine("[2] 로비로 돌아가기");
-
-                string input = Console.ReadLine();
-                if (input == "1")
-                {
-                    EnterField();
-                }
-                else if (input == "2")
-                {
-                    break;
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            while (true)
-            {
-                ClassType choice = ClassType.None;
-                choice=ChooseClass();
-                if (choice != ClassType.None)
-                {
-                    Player player;
-                    CreatPlayer(choice,out player);
-                    Console.WriteLine($"Hp{player.hp} Attack{player.attack}");
-
-                    EnterGame();
-                }
-            }
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 10;
+            
+            knight.Move();
+            knight.Attack();
         }
     }
 }
