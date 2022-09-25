08f9ed366dd4bc10afbef1a1f539d5ff2b801f8e
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-09-25 18:01:42 +0900

    복사(값)와 참조

--- test/test/test.cs
+++ test/test/test.cs
@@ -9,6 +9,13 @@ namespace CSharp
         public int hp;
         public int attack;
 
+        public Knight Clone()
+        {
+            Knight knight = new Knight();
+            knight.hp = hp;
+            knight.attack = hp;
+            return knight;
+        }
         public void Move()
         {
             Console.WriteLine("Knight Move");
@@ -19,16 +26,38 @@ namespace CSharp
             Console.WriteLine("Knight Attack");
         }
     }
+
+    struct Mage
+    {
+        public int hp;
+        public int attack;
+    }
     class Program
     {
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+        
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0;
+        }
         static void Main(string[] args)
         {
+            Mage mage;
+            mage.hp = 100;
+            mage.attack = 50;
+
+            Mage mage2 = mage;
+            mage2.hp = 0;
+            
             Knight knight = new Knight();
             knight.hp = 100;
             knight.attack = 10;
-            
-            knight.Move();
-            knight.Attack();
+
+            Knight knight2 = knight.Clone();
+            knight2.hp = 0;
         }
     }
 }
