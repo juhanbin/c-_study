96fc978eeb3987228c50d5d50f390f266ff09400
juhanbin <gksqls0509@naver.com>
2022-10-05 18:07:38 +0900

    상속성

--- test/test/test.cs
+++ test/test/test.cs
@@ -4,59 +4,39 @@ using System.Runtime.InteropServices;
 
 namespace CSharp
 {
-    class Knight
+    class Player // 부모 / 기반
     {
         static public int counter = 1;//오로지 1개만 존재
-        
+        public int id;
         public int hp;
         public int attack;
 
-        static public void Test()
-        {
-            counter++;
-        }
-
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 1;
-            return knight;
-        }
-        public Knight()
-        {
-            hp = 100;
-            attack = 10;
-            Console.WriteLine("생성자 호출");
-        }
-
-        public Knight Clone()
-        {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = hp;
-            return knight;
-        }
-        
         public void Move()
         {
-            Console.WriteLine("Knight Move");
+            Console.WriteLine("Player Move");
         }
 
         public void Attack()
         {
-            Console.WriteLine("Knight Attack");
+            Console.WriteLine("Player Attack");
         }
     }
-    
+    class Mage : Player
+    {
+    }
+    class Archer : Player
+    {
+    }
+    class Knight : Player // 자식 / 파생
+    {
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Knight knight = Knight.CreateKnight();//static
-            knight.Move();//일반
-
-            Console.WriteLine();
+            Knight knight = new Knight();
+            knight.Move();
         }
     }
 }
