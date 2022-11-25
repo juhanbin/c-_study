e0ff1e8ad1e6f13b1eb7507b04ca934ca0ea639c
juhanbin <gksqls0509@naver.com>
2022-11-25 23:43:36 +0900

    static

--- test/test/test.cs
+++ test/test/test.cs
@@ -13,11 +13,32 @@ namespace CSharp
     //참조
     class Knight
     {
+        //필드
+        static public int counter = 1;    //오로지 1개만 존재
+
+        public int id;
         public int hp;
         public int attack;
+        /*
+        static public void Test()   //static O 붕어빵틀에 종속 / static X 인스턴스에 종속
+        {
+            counter++;
+        }
+        */
+
+        static public Knight CreateKnight()
+        {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 1;
+            return knight;
+        }
 
         public Knight()
         {
+            id = counter;
+            counter++;
+            
             hp = 100;
             attack = 10;
             Console.WriteLine("생성자 호출");
@@ -78,11 +99,11 @@ namespace CSharp
 
             Mage mage2 = mage;
             mage2.hp = 0;
-            */
+            
             Knight knight = new Knight();   //객체 생성
             Knight knight2 = new Knight(50);
             Knight knight3 = new Knight(50,30);
-            /*
+            
             knight.hp=100;
             knight.attack = 10;
             knight.Move();
@@ -91,6 +112,13 @@ namespace CSharp
 
             Knight knight2 = knight.Clone();
             knight2.hp = 0;
+            */
+            Knight knight = Knight.CreateKnight();  //static O
+            knight.Move();  //static X
+            /*
+            Console.WriteLine();
+            Random rand = new Random();     //static O
+            rand.Next(0,2);     //static X
             */
         }
     }
